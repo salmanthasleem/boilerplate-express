65f46b35306d0257ccd186d96acd7c1514cda1f3
Salman Thasleem <salmanthasleem@gmail.com>
2021-08-10 19:06:44 +0530

    Section 2 Task 3

--- myApp.js
+++ myApp.js
@@ -2,11 +2,11 @@ var express = require("express");
 const path = require("path");
 var app = express();
 
-app.use(express.static("./public"));
-
 app.get("/", (req, res) => {
   const absolutePath = path.resolve(__dirname, "./views/index.html");
   res.sendFile(absolutePath);
 });
+
+app.use(express.static("./public"));
 
 module.exports = app;
