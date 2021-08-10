aba900f78a9779bb28962cbb3beb970bd4c7af7d
Salman Thasleem <salmanthasleem@gmail.com>
2021-08-10 20:19:32 +0530

    Section 2 Task 6

--- myApp.js
+++ myApp.js
@@ -11,16 +11,12 @@ app.get("/", (req, res) => {
 });
 
 app.get("/json", (req, res) => {
-<<<<<<< HEAD
   let messageTxt = "Hello Json";
   const { message } =
     process.env.MESSAGE_STYLE === uppercase
       ? messageTxt.toLocaleUpperCase()
       : messageTxt;
   res.json(message);
-=======
-  res.json({ message: "Hello json" });
->>>>>>> b573f3fe3bf943cd54d9be8899dca3feab365e10
 });
 
 module.exports = app;
